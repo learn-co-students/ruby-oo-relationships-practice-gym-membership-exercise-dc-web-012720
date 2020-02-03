ad98935780a4690a2945262d4715492398283925
github username <github email address>
2020-02-03 17:25:58 -0500

    done

--- lib/gym.rb
+++ lib/gym.rb
@@ -1,7 +1,32 @@
 class Gym
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def memberships
+    Membership.all.select { |membership| membership.gym == self}
+  end 
+
+  def lifters
+    self.memberships.map { |membership| membership.lifter}
+  end 
+
+  def lifters_name
+    self.lifters.map { |lifter| lifter.name}
+  end 
+
+  def total_lift
+    self.lifters.sum { |lifter| lifter.lift_total}
+  end 
+
+
+  def self.all
+    @@all
+  end 
+
 end
--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,36 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    @@all << self
   end
+
+  def memberships 
+    Membership.all.select { |membership| membership.lifter == self }
+  end 
+
+  def gyms
+    self.memberships.map { |membership| membership.gym }
+  end
+  
+  def self.avg_lift_total
+    total_avg_lift = self.all.map {|lift| lifter.lift_total}.sum
+    total_avg_lift / self.all.count
+  end 
+
+  def total_cost_membership
+    self.memberships.sum { |membership| membership.cost}
+  end 
+
+  def change_gym(cost, gym)
+    Membership.new(cost, self, gym)
+  end 
+
+  def self.all
+    @@all
+  end 
 end
